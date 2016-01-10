aa933ff40b4c7a0370832d1998cf764087f12b08
Tomivav <tomivav@gmail.com>
2016-01-10 23:44:13 +0100

    Basic implementation of Google Sign In

--- app/src/main/java/com/plazonic/tomislav/yambfriends/SignInActivity.java
+++ app/src/main/java/com/plazonic/tomislav/yambfriends/SignInActivity.java
@@ -1,28 +1,124 @@
 package com.plazonic.tomislav.yambfriends;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
 
-public class SignInActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
+public class SignInActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, View.OnClickListener {
+
+    private static final String TAG = "SignInActivity";
+    private static final int RC_SIGN_IN = 9001;
+
+    private GoogleApiClient googleApiClient;
+    private TextView tvAccountInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
 
+        tvAccountInfo = (TextView) findViewById(R.id.account_info);
+
+        findViewById(R.id.google_sign_in_button).setOnClickListener(this);
+        findViewById(R.id.google_sign_out_button).setOnClickListener(this);
+        findViewById(R.id.google_disconnect_button).setOnClickListener(this);
+
         GoogleSignInOptions googleSignInOptions =
                 new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).build();
-        GoogleApiClient googleApiClient =
+        googleApiClient =
                 new GoogleApiClient.Builder(this).enableAutoManage(this, this).addApi(Auth.GOOGLE_SIGN_IN_API, googleSignInOptions).build();
     }
 
     @Override
     public void onConnectionFailed(ConnectionResult connectionResult) {
-
+        Log.d(TAG, "onConnectionFailed: " + connectionResult);
     }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.google_sign_in_button:
+                googleSignIn();
+                break;
+            case R.id.google_sign_out_button:
+                googleSignOut();
+                break;
+            case R.id.google_disconnect_button:
+                googleRevokeAccess();
+                break;
+            /*case R.id.facebook_sign_in_button:
+                break;*/
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult googleSignInResult = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleGoogleSignInResult(googleSignInResult);
+        }
+    }
+
+    private void googleSignIn() {
+        Intent googleSignInIntent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
+        startActivityForResult(googleSignInIntent, RC_SIGN_IN);
+    }
+
+    private void googleSignOut() {
+        Auth.GoogleSignInApi.signOut(googleApiClient).setResultCallback(
+                new ResultCallback<Status>() {
+                    @Override
+                    public void onResult(Status status) {
+                        updateUI(false);
+                    }
+                }
+        );
+    }
+
+    private void googleRevokeAccess() {
+        Auth.GoogleSignInApi.revokeAccess(googleApiClient).setResultCallback(
+                new ResultCallback<Status>() {
+                    @Override
+                    public void onResult(Status status) {
+                        updateUI(false);
+                    }
+                }
+        );
+    }
+
+    private void handleGoogleSignInResult (GoogleSignInResult googleSignInResult) {
+        if (googleSignInResult.isSuccess()) {
+            GoogleSignInAccount googleSignInAccount = googleSignInResult.getSignInAccount();
+            tvAccountInfo.setText(googleSignInAccount.getDisplayName() + ", " + googleSignInAccount.getId());
+            updateUI(true);
+        } else {
+            updateUI(false);
+        }
+    }
+
+    private void updateUI(boolean signedIn) {
+        if (signedIn) {
+            findViewById(R.id.google_sign_in_button).setVisibility(View.GONE);
+            findViewById(R.id.google_sign_out_disconnect).setVisibility(View.VISIBLE);
+        } else {
+            findViewById(R.id.google_sign_out_disconnect).setVisibility(View.GONE);
+            findViewById(R.id.google_sign_in_button).setVisibility(View.VISIBLE);
+        }
+    }
+
 }
