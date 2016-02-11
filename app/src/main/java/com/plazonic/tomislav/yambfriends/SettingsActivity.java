4dab1e803faec9e03cd5cf3a29477ed9b46458a3
tomiplaz <tomislav.plazonic@gmail.com>
2016-02-11 13:39:34 +0100

    Fix Settings layout

--- app/src/main/java/com/plazonic/tomislav/yambfriends/SettingsActivity.java
+++ app/src/main/java/com/plazonic/tomislav/yambfriends/SettingsActivity.java
@@ -8,12 +8,7 @@ public class SettingsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // Display the fragment as the main content.
-        getFragmentManager()
-            .beginTransaction()
-            .replace(android.R.id.content, new SettingsFragment())
-            .commit();
+        setContentView(R.layout.activity_settings);
     }
 
 }
